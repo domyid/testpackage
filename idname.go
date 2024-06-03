e13fcbc3a2b9b36cfe5af3e90547655ed588902a
fahiraaa06 <fahira1678@gmail.com>
2024-06-03 15:46:27 +0700

    test webhook

--- idname.go
+++ idname.go
@@ -4,5 +4,5 @@ import "github.com/whatsauth/itmodel"
 
 func IDName(Pesan itmodel.IteungMessage) (reply string) {
 
-	return "Hai.. hai..  kakak atas nama :\n" + Pesan.Alias_name + "\n berhasil absensi\nmakasih"
+	return "Hai.. hai..  kakak atas nama :\n" + Pesan.Alias_name + "\n berhasil absen\nmakasih"
 }
